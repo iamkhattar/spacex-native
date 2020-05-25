2dadeaf4cc52f4dad68939d98f5e11d8122afb70
iamkhattar <shivamkhattar1@gmail.com>
2020-05-25 20:52:42 +0100

    Card Initialisation

--- client/components/Launches/LaunchesLanding.js
+++ client/components/Launches/LaunchesLanding.js
@@ -1,12 +1,59 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  ScrollView,
+  Image,
+  Text,
+} from "react-native";
 
 const LaunchesLanding = () => {
   return (
-    <View>
-      <Text>LaunchesLanding</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoWrapper}>
+        <Image
+          style={styles.logoStyle}
+          source={require("../../assets/spacex-logo.png")}
+        />
+      </View>
+      <View style={styles.contentWrapper}>
+        <ScrollView style={styles.scrollViewWrapper}>
+          <View style={styles.cardStyle}>
+            <Text>Hi</Text>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1E1E24",
+  },
+  logoWrapper: {
+    flex: 1,
+  },
+  contentWrapper: {
+    flex: 6,
+  },
+  logoStyle: {
+    resizeMode: "contain",
+    width: "100%",
+  },
+  scrollViewWrapper: {
+    flex: 1,
+  },
+  cardStyle: {
+    flex: 1,
+    backgroundColor: "grey",
+    margin: 7,
+    padding: 5,
+    borderRadius: 10,
+    height: 250,
+  },
+});
 
 export default LaunchesLanding;
